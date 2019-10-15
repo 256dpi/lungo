25398aac8d4eeeff2152c446806e728ff3a04ec5
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-10-16 00:08:14 +0200

    renamed file

---
+++ native.go
@@ -0,0 +1,84 @@
+package lungo
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var _ Client = &NativeClient{}
+
+type NativeClient struct {
+	*mongo.Client
+}
+
+func Connect(ctx context.Context, opts ...*options.ClientOptions) (Client, error) {
+	client, err := mongo.Connect(ctx, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	return &NativeClient{Client: client}, nil
+}
+
+func (c *NativeClient) Database(name string, opts ...*options.DatabaseOptions) Database {
+	return &NativeDatabase{Database: c.Client.Database(name, opts...), client: c}
+}
+
+var _ Database = &NativeDatabase{}
+
+type NativeDatabase struct {
+	*mongo.Database
+
+	client *NativeClient
+}
+
+func (m *NativeDatabase) Aggregate(ctx context.Context, pipeline interface{}, opts ...*options.AggregateOptions) (Cursor, error) {
+	return m.Database.Aggregate(ctx, pipeline, opts...)
+}
+
+func (m *NativeDatabase) Client() Client {
+	return m.client
+}
+
+func (m *NativeDatabase) Collection(name string, opts ...*options.CollectionOptions) Collection {
+	return &NativeCollection{Collection: m.Database.Collection(name, opts...)}
+}
+
+func (m *NativeDatabase) ListCollections(ctx context.Context, filter interface{}, opts ...*options.ListCollectionsOptions) (Cursor, error) {
+	return m.Database.ListCollections(ctx, filter, opts...)
+}
+
+func (m *NativeDatabase) RunCommandCursor(ctx context.Context, filter interface{}, opts ...*options.RunCmdOptions) (Cursor, error) {
+	return m.Database.RunCommandCursor(ctx, filter, opts...)
+}
+
+var _ Collection = &NativeCollection{}
+
+type NativeCollection struct {
+	*mongo.Collection
+
+	db *NativeDatabase
+}
+
+func (c *NativeCollection) Aggregate(ctx context.Context, pipeline interface{}, opts ...*options.AggregateOptions) (Cursor, error) {
+	return c.Collection.Aggregate(ctx, pipeline, opts...)
+}
+
+func (c *NativeCollection) Clone(opts ...*options.CollectionOptions) (Collection, error) {
+	coll, err := c.Collection.Clone(opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	return &NativeCollection{Collection: coll, db: c.db}, nil
+}
+
+func (c *NativeCollection) Database() Database {
+	return c.db
+}
+
+func (c *NativeCollection) Find(ctx context.Context, filter interface{}, opts ...*options.FindOptions) (Cursor, error) {
+	return c.Collection.Find(ctx, filter, opts...)
+}
