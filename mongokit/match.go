4ea047f41d82aa580dd283ad7589c6dd0be7dda1
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-10-17 20:38:39 +0200

    improved matching

--- mongokit/match.go
+++ mongokit/match.go
@@ -10,23 +10,30 @@ import (
 
 // https://github.com/mongodb/mongo/blob/master/src/mongo/db/matcher/expression_leaf.cpp
 
-var Matchers = map[string]func(bson.D, string, interface{}) (bool, error){}
+type Operator func(bson.D, string, interface{}) (bool, error)
+
+var TopLevelQueryOperators = map[string]Operator{}
+var ExpressionQueryOperators = map[string]Operator{}
 
 func init() {
 	// TODO: Add more operators.
 
-	// register logical matchers
-	Matchers["$and"] = matchAnd
-	Matchers["$not"] = matchNot
-	Matchers["$nor"] = matchNor
-	Matchers["$or"] = matchOr
+	// register top level query operators matchers
+	TopLevelQueryOperators["$and"] = matchAnd
+	TopLevelQueryOperators["$not"] = matchNot
+	TopLevelQueryOperators["$nor"] = matchNor
+	TopLevelQueryOperators["$or"] = matchOr
 
-	// register comparison matchers
-	Matchers["$eq"] = matchComp("$eq")
-	Matchers["$gt"] = matchComp("$gt")
-	Matchers["$lt"] = matchComp("$lt")
-	Matchers["$gte"] = matchComp("$gte")
-	Matchers["$lte"] = matchComp("$lte")
+	// register expression query operators
+	ExpressionQueryOperators["$and"] = matchAnd
+	ExpressionQueryOperators["$not"] = matchNot
+	ExpressionQueryOperators["$nor"] = matchNor
+	ExpressionQueryOperators["$or"] = matchOr
+	ExpressionQueryOperators["$eq"] = matchComp("$eq")
+	ExpressionQueryOperators["$gt"] = matchComp("$gt")
+	ExpressionQueryOperators["$lt"] = matchComp("$lt")
+	ExpressionQueryOperators["$gte"] = matchComp("$gte")
+	ExpressionQueryOperators["$lte"] = matchComp("$lte")
 }
 
 func Match(doc, query bson.D) (bool, error) {
@@ -43,52 +50,57 @@ func Match(doc, query bson.D) (bool, error) {
 	return true, nil
 }
 
-// the query document may contain root matchers "{ $and: [], ...}", field matchers
-// "{ field: { $exp: value }, ... }" and simple equality matchers "{ field: value }, ... }"
-
-// expressions documents however may only contain expressions "$exp: value, ..."
-
 func matchQueryPair(doc bson.D, pair bson.E) (bool, error) {
-	// handle root matchers e.g. $and, $or, ...
-	rootMatcher := Matchers[pair.Key]
-	if rootMatcher != nil {
-		return rootMatcher(doc, "", pair.Value)
+	// call top level query operators which may appear together with field
+	// expressions in the query filter document
+	operator := TopLevelQueryOperators[pair.Key]
+	if operator != nil {
+		return operator(doc, "", pair.Value)
 	}
 
-	// check document matcher "{ field: { $exp: value } }"
+	// check for field expressions with a document which may contain either
+	// only expression query operators or only simple equality conditions
 	if exps, ok := pair.Value.(bson.D); ok {
-		// check if there are operators
-		found := false
-		for _, exp := range exps {
-			if exp.Key[0] == '$' {
-				found = true
-			}
-		}
-
 		// match all expressions if found (implicit and)
-		if found {
-			for _, exp := range exps {
-				// lookup matcher
-				matcher := Matchers[exp.Key]
-				if matcher == nil {
-					return false, fmt.Errorf("match: unkown matcher %q", exp.Key)
-				}
-
-				// call matcher
-				ok, err := matcher(doc, pair.Key, exp.Value)
-				if err != nil {
-					return false, err
-				} else if !ok {
-					return false, nil
-				}
+		for i, exp := range exps {
+			// break and leave document fo the default equality operator if the
+			// first key does not look like an operator
+			if i == 0 && len(exp.Key) > 0 && exp.Key[0] != '$' {
+				break
 			}
 
-			return true, nil
+			// lookup operator
+			operator := ExpressionQueryOperators[exp.Key]
+			if operator == nil && i == 0 {
+				break
+			} else if operator == nil {
+				return false, fmt.Errorf("match: unkown operator %q", exp.Key)
+			}
+
+			// call matcher
+			ok, err := operator(doc, pair.Key, exp.Value)
+			if err != nil {
+				return false, err
+			} else if !ok {
+				return false, nil
+			}
 		}
+
+		return true, nil
 	}
 
-	// use default equality matcher "{ field: value } }"
-	return Matchers["$eq"](doc, pair.Key, pair.Value)
+	// handle pair as a simple equality condition
+
+	// get the equality query operator
+	operator = ExpressionQueryOperators["$eq"]
+	if operator == nil {
+		return false, fmt.Errorf("match: missing default equality operator")
+	}
+
+	// call equality query operator
+	res, err := operator(doc, pair.Key, pair.Value)
+
+	return res, err
 }
 
 func matchAnd(doc bson.D, _ string, v interface{}) (bool, error) {
@@ -96,6 +108,11 @@ func matchAnd(doc bson.D, _ string, v interface{}) (bool, error) {
 	list, ok := v.(bson.A)
 	if !ok {
 		return false, fmt.Errorf("match: $and: expected list")
+	}
+
+	// check list
+	if len(list) == 0 {
+		return false, fmt.Errorf("match: $and: empty list")
 	}
 
 	// match all expressions
@@ -141,6 +158,11 @@ func matchNor(doc bson.D, _ string, v interface{}) (bool, error) {
 		return false, fmt.Errorf("match: $nor: expected list")
 	}
 
+	// check list
+	if len(list) == 0 {
+		return false, fmt.Errorf("match: $and: empty list")
+	}
+
 	// match first item
 	for _, item := range list {
 		// coerce item
@@ -168,6 +190,11 @@ func matchOr(doc bson.D, _ string, v interface{}) (bool, error) {
 		return false, fmt.Errorf("match: $or: expected list")
 	}
 
+	// check list
+	if len(list) == 0 {
+		return false, fmt.Errorf("match: $and: empty list")
+	}
+
 	// match first item
 	for _, item := range list {
 		// coerce item
@@ -188,7 +215,7 @@ func matchOr(doc bson.D, _ string, v interface{}) (bool, error) {
 	return false, nil
 }
 
-func matchComp(typ string) func(bson.D, string, interface{}) (bool, error) {
+func matchComp(op string) Operator {
 	return func(doc bson.D, path string, v interface{}) (bool, error) {
 		// get field value
 		field := bsonkit.Get(doc, path)
@@ -202,8 +229,8 @@ func matchComp(typ string) func(bson.D, string, interface{}) (bool, error) {
 			return false, err
 		}
 
-		// check type
-		switch typ {
+		// check operator
+		switch op {
 		case "$eq":
 			return res == 0, nil
 		case "$gt":
@@ -214,8 +241,8 @@ func matchComp(typ string) func(bson.D, string, interface{}) (bool, error) {
 			return res < 0, nil
 		case "$lte":
 			return res <= 0, nil
+		default:
+			return false, fmt.Errorf("match: unkown comparison operator %q", op)
 		}
-
-		return false, fmt.Errorf("match: unreachable")
 	}
 }
--- mongokit/match_test.go
+++ mongokit/match_test.go
@@ -9,24 +9,48 @@ import (
 	"github.com/256dpi/lungo/bsonkit"
 )
 
-func matchTest(t *testing.T, doc, query bson.M, result bool) {
+func matchTest(t *testing.T, doc, query bson.M, result interface{}) {
 	t.Run("Mongo", func(t *testing.T) {
 		coll := testCollection()
 		_, err := coll.InsertOne(nil, doc)
 		assert.NoError(t, err)
 		n, err := coll.CountDocuments(nil, query)
-		assert.NoError(t, err)
-		assert.Equal(t, result, n == 1, query)
+		if result == nil {
+			assert.Error(t, err, query)
+			assert.Zero(t, n, query)
+		} else {
+			assert.NoError(t, err)
+			assert.Equal(t, result, n == 1, query)
+		}
 	})
 
 	t.Run("Lungo", func(t *testing.T) {
 		res, err := Match(bsonkit.Convert(doc), bsonkit.Convert(query))
-		assert.NoError(t, err)
-		assert.Equal(t, result, res, query)
+		if result == nil {
+			assert.Error(t, err, query)
+			assert.False(t, res, query)
+		} else {
+			assert.NoError(t, err)
+			assert.Equal(t, result, res, query)
+		}
 	})
 }
 
-func TestMatchValidEq(t *testing.T) {
+func TestMatchBasic(t *testing.T) {
+	// empty query filter
+	matchTest(t, bson.M{
+		"foo": "bar",
+	}, bson.M{}, true)
+
+	// empty top level and operator
+	matchTest(t, bson.M{
+		"foo": "bar",
+	}, bson.M{
+		"$and": bson.A{},
+	}, nil)
+}
+
+func TestMatchEq(t *testing.T) {
 	matchTest(t, bson.M{
 		"foo": "bar",
 	}, bson.M{
@@ -65,6 +89,18 @@ func TestMatchValidEq(t *testing.T) {
 		"foo": bson.M{
 			"bar": bson.M{
 				"$eq": "baz",
+			},
+		},
+	}, true)
+
+	matchTest(t, bson.M{
+		"foo": bson.M{
+			"bar": "baz",
+		},
+	}, bson.M{
+		"foo": bson.M{
+			"$eq": bson.M{
+				"bar": "baz",
 			},
 		},
 	}, true)
