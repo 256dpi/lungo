8bd767197cc76b97dcdadaa34f3bc21d9eee6466
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-10-18 11:21:07 +0200

    added note

--- utils.go
+++ utils.go
@@ -2,6 +2,8 @@ package lungo
 
 import "fmt"
 
+// TODO: Add reflect based test to catch also added options.
+
 func assertUnsupported(features map[string]bool) error {
 	for name, unsupported := range features {
 		if unsupported {
