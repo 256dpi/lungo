2bdecb644465dff9194c81f235d484949b0dce27
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-10-23 15:32:42 +0200

    use process framework for extract

--- mongokit/extract.go
+++ mongokit/extract.go
@@ -10,10 +10,8 @@ import (
 
 // https://github.com/mongodb/mongo/blob/master/src/mongo/db/extracter/expression_leaf.cpp
 
-type ExtractOperator func(bsonkit.Doc, string, interface{}) error
-
-var TopLevelExtractOperators = map[string]ExtractOperator{}
-var ExpressionExtractOperators = map[string]ExtractOperator{}
+var TopLevelExtractOperators = map[string]Operator{}
+var ExpressionExtractOperators = map[string]Operator{}
 
 func init() {
 	// register top level extractors
@@ -30,8 +28,11 @@ func Extract(query bsonkit.Doc) (bsonkit.Doc, error) {
 	// prepare doc
 	doc := &bson.D{}
 
-	// extract query
-	err := extractAll(doc, *query, true)
+	// match document to query
+	err := Process(&Context{
+		TopLevel:   TopLevelExtractOperators,
+		Expression: ExpressionExtractOperators,
+	}, doc, *query, true)
 	if err != nil {
 		return nil, err
 	}
@@ -39,102 +40,16 @@ func Extract(query bsonkit.Doc) (bsonkit.Doc, error) {
 	return doc, nil
 }
 
-func extractAll(doc bsonkit.Doc, query bson.D, root bool) error {
-	// extract all expressions
-	for _, exp := range query {
-		err := extractExpression(doc, "", exp, root)
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-func extractExpression(doc bsonkit.Doc, prefix string, pair bson.E, root bool) error {
-	// check for top level operators which may appear together with field
-	// expressions in the query document
-	if len(pair.Key) > 0 && pair.Key[0] == '$' {
-		// lookup top level operator
-		var operator ExtractOperator
-		if root {
-			operator = TopLevelExtractOperators[pair.Key]
-		} else {
-			operator = ExpressionExtractOperators[pair.Key]
-		}
-		if operator == nil {
-			return fmt.Errorf("extract: unknown top level operator %q", pair.Key)
-		}
-
-		// call operator
-		return operator(doc, prefix, pair.Value)
-	}
-
-	// get path
-	path := pair.Key
-	if prefix != "" {
-		path = prefix + "." + path
-	}
-
-	// check for field expressions with a document which may contain either
-	// only expression operators or only simple equality conditions
-	if exps, ok := pair.Value.(bson.D); ok {
-		// extract all expressions if found
-		for i, exp := range exps {
-			// break and leave document as a simple equality condition if the
-			// first key does not look like an operator
-			if i == 0 && (len(exp.Key) == 0 || exp.Key[0] != '$') {
-				break
-			}
-
-			// check operator validity
-			if len(exp.Key) == 0 || exp.Key[0] != '$' {
-				return fmt.Errorf("extract: expected operator, got %q", exp.Key)
-			}
-
-			// lookup operator
-			operator := ExpressionExtractOperators[exp.Key]
-			if operator == nil {
-				return fmt.Errorf("extract: unknown operator %q", exp.Key)
-			}
-
-			// call operator
-			err := operator(doc, path, exp.Value)
-			if err != nil {
-				return err
-			}
-
-			// return success if last one
-			if i == len(exps)-1 {
-				return nil
-			}
-		}
-	}
-
-	// handle pair as a simple equality condition
-
-	// get the equality query operator
-	operator := ExpressionExtractOperators[""]
-	if operator == nil {
-		return fmt.Errorf("extract: missing default equality operator")
-	}
-
-	// call equality operator
-	err := operator(doc, path, pair.Value)
-
-	return err
-}
-
-func extractAnd(doc bsonkit.Doc, _ string, v interface{}) error {
+func extractAnd(ctx *Context, doc bsonkit.Doc, _ string, v interface{}) error {
 	// get array
 	list, ok := v.(bson.A)
 	if !ok {
-		return fmt.Errorf("extract: $and: expected list")
+		return fmt.Errorf("$and: expected list")
 	}
 
 	// check list
 	if len(list) == 0 {
-		return fmt.Errorf("extract: $and: empty list")
+		return fmt.Errorf("$and: empty list")
 	}
 
 	// extract all expressions
@@ -142,11 +57,11 @@ func extractAnd(doc bsonkit.Doc, _ string, v interface{}) error {
 		// coerce item
 		query, ok := item.(bson.D)
 		if !ok {
-			return fmt.Errorf("extract: $and: expected list of documents")
+			return fmt.Errorf("$and: expected list of documents")
 		}
 
 		// extract document
-		err := extractAll(doc, query, false)
+		err := Process(ctx, doc, query, false)
 		if err != nil {
 			return err
 		}
@@ -155,16 +70,16 @@ func extractAnd(doc bsonkit.Doc, _ string, v interface{}) error {
 	return nil
 }
 
-func extractOr(doc bsonkit.Doc, _ string, v interface{}) error {
+func extractOr(ctx *Context, doc bsonkit.Doc, _ string, v interface{}) error {
 	// get array
 	list, ok := v.(bson.A)
 	if !ok {
-		return fmt.Errorf("extract: $or: expected list")
+		return fmt.Errorf("$or: expected list")
 	}
 
 	// check list
 	if len(list) == 0 {
-		return fmt.Errorf("extract: $or: empty list")
+		return fmt.Errorf("$or: empty list")
 	}
 
 	// check list
@@ -177,11 +92,11 @@ func extractOr(doc bsonkit.Doc, _ string, v interface{}) error {
 		// coerce item
 		query, ok := item.(bson.D)
 		if !ok {
-			return fmt.Errorf("extract: $or: expected list of documents")
+			return fmt.Errorf("$or: expected list of documents")
 		}
 
 		// extract document
-		err := extractAll(doc, query, false)
+		err := Process(ctx, doc, query, false)
 		if err != nil {
 			return err
 		}
@@ -190,15 +105,15 @@ func extractOr(doc bsonkit.Doc, _ string, v interface{}) error {
 	return nil
 }
 
-func extractEq(doc bsonkit.Doc, path string, v interface{}) error {
+func extractEq(ctx *Context, doc bsonkit.Doc, path string, v interface{}) error {
 	return bsonkit.Put(doc, path, v, false)
 }
 
-func extractIn(doc bsonkit.Doc, path string, v interface{}) error {
+func extractIn(ctx *Context, doc bsonkit.Doc, path string, v interface{}) error {
 	// get array
 	list, ok := v.(bson.A)
 	if !ok {
-		return fmt.Errorf("extract: $in: expected list")
+		return fmt.Errorf("$in: expected list")
 	}
 
 	// check list
