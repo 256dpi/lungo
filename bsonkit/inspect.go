21a69bb558d1d0951d44d2769e9b8afae7ff00de
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-10-26 18:33:39 +0200

    added maps for reversing types

--- bsonkit/inspect.go
+++ bsonkit/inspect.go
@@ -26,6 +26,42 @@ const (
 	Regex
 )
 
+var TypeString = map[string]bsontype.Type{}
+var TypeNumber = map[byte]bsontype.Type{}
+
+func init() {
+	// make list
+	types := []bsontype.Type{
+		bsontype.Double,
+		bsontype.String,
+		bsontype.EmbeddedDocument,
+		bsontype.Array,
+		bsontype.Binary,
+		bsontype.Undefined,
+		bsontype.ObjectID,
+		bsontype.Boolean,
+		bsontype.DateTime,
+		bsontype.Null,
+		bsontype.Regex,
+		bsontype.DBPointer,
+		bsontype.JavaScript,
+		bsontype.Symbol,
+		bsontype.CodeWithScope,
+		bsontype.Int32,
+		bsontype.Timestamp,
+		bsontype.Int64,
+		bsontype.Decimal128,
+		bsontype.MinKey,
+		bsontype.MaxKey,
+	}
+
+	// fill maps
+	for _, t := range types {
+		TypeString[t.String()] = t
+		TypeNumber[byte(t)] = t
+	}
+}
+
 func Inspect(v interface{}) (Type, bsontype.Type) {
 	switch v.(type) {
 	case nil, primitive.Null, MissingType:
