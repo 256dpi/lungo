70956cc87ab7b8878e096cf2b49c3ed2fc386bad
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-10-28 14:28:13 +0100

    added store docs

--- store.go
+++ store.go
@@ -10,35 +10,42 @@ import (
 	"github.com/256dpi/lungo/dbkit"
 )
 
+// Store is the interface that describes storage adapters.
 type Store interface {
 	Load() (*Dataset, error)
 	Store(*Dataset) error
 }
 
+// MemoryStore holds the dataset in memory.
 type MemoryStore struct {
 	dataset *Dataset
 }
 
+// NewMemoryStore creates and returns a new memory store.
 func NewMemoryStore() *MemoryStore {
 	return &MemoryStore{
 		dataset: NewDataset(),
 	}
 }
 
+// Load will return the dataset.
 func (m MemoryStore) Load() (*Dataset, error) {
 	return m.dataset, nil
 }
 
+// Store will store the dataset.
 func (m MemoryStore) Store(data *Dataset) error {
 	m.dataset = data
 	return nil
 }
 
+// SingleFileStore writes the dataset to a single file on disk.
 type SingleFileStore struct {
 	path string
 	mode os.FileMode
 }
 
+// NewSingleFileStore creates and returns a new single file store.
 func NewSingleFileStore(path string, mode os.FileMode) *SingleFileStore {
 	return &SingleFileStore{
 		path: path,
@@ -46,6 +53,7 @@ func NewSingleFileStore(path string, mode os.FileMode) *SingleFileStore {
 	}
 }
 
+// Load will read the dataset from disk and return it.
 func (s *SingleFileStore) Load() (*Dataset, error) {
 	// load file
 	buf, err := ioutil.ReadFile(s.path)
@@ -68,6 +76,7 @@ func (s *SingleFileStore) Load() (*Dataset, error) {
 	return &dataset, nil
 }
 
+// Store will atomically write the dataset to disk.
 func (s *SingleFileStore) Store(data *Dataset) error {
 	// encode dataset
 	buf, err := bson.Marshal(data)
