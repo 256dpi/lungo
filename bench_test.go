b0342a9f901b9526f4edfcc026f55e4e555b29b4
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-10-28 21:37:07 +0100

    updated benchmarks

--- bench_test.go
+++ bench_test.go
@@ -1,13 +1,14 @@
 package lungo
 
 import (
+	"math/rand"
 	"os"
 	"testing"
 
 	"go.mongodb.org/mongo-driver/bson"
 )
 
-func BenchmarkMemoryStore(b *testing.B) {
+func BenchmarkMemoryStoreWrite(b *testing.B) {
 	client, engine, err := Open(nil, Options{
 		Store: NewMemoryStore(),
 	})
@@ -17,17 +18,57 @@ func BenchmarkMemoryStore(b *testing.B) {
 
 	defer engine.Close()
 
-	doc := bson.M{"foo": "bar"}
-
 	for i := 0; i < b.N; i++ {
-		_, err = client.Database("foo").Collection("foo").InsertOne(nil, doc)
+		_, err = client.Database("foo").Collection("foo").InsertOne(nil, bson.M{
+			"n": i,
+		})
+		if err != nil {
+			panic(err)
+		}
+
+		_, err = client.Database("foo").Collection("foo").DeleteMany(nil, bson.M{
+			"n": bson.M{
+				"$lt": i - 1000,
+			},
+		})
 		if err != nil {
 			panic(err)
 		}
 	}
 }
 
-func BenchmarkSingleFileStore(b *testing.B) {
+func BenchmarkMemoryStoreRead(b *testing.B) {
+	client, engine, err := Open(nil, Options{
+		Store: NewMemoryStore(),
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	defer engine.Close()
+
+	for i := 0; i < 100; i++ {
+		_, err = client.Database("foo").Collection("foo").InsertOne(nil, bson.M{
+			"n": i,
+		})
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_, err = client.Database("foo").Collection("foo").Find(nil, bson.M{
+			"n": rand.Intn(100),
+		})
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func BenchmarkSingleFileStoreWrite(b *testing.B) {
 	_ = os.Remove("./bench.bson")
 
 	client, engine, err := Open(nil, Options{
@@ -39,10 +80,52 @@ func BenchmarkSingleFileStore(b *testing.B) {
 
 	defer engine.Close()
 
-	doc := bson.M{"foo": "bar"}
+	for i := 0; i < b.N; i++ {
+		_, err = client.Database("foo").Collection("foo").InsertOne(nil, bson.M{
+			"n": i,
+		})
+		if err != nil {
+			panic(err)
+		}
+
+		_, err = client.Database("foo").Collection("foo").DeleteMany(nil, bson.M{
+			"n": bson.M{
+				"$lt": i - 100,
+			},
+		})
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func BenchmarkFileStoreRead(b *testing.B) {
+	_ = os.Remove("./bench.bson")
+
+	client, engine, err := Open(nil, Options{
+		Store: NewFileStore("./bench.bson", 0666),
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	defer engine.Close()
+
+	for i := 0; i < 100; i++ {
+		_, err = client.Database("foo").Collection("foo").InsertOne(nil, bson.M{
+			"n": i,
+		})
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
-		_, err = client.Database("foo").Collection("foo").InsertOne(nil, doc)
+		_, err = client.Database("foo").Collection("foo").Find(nil, bson.M{
+			"n": rand.Intn(100),
+		})
 		if err != nil {
 			panic(err)
 		}
