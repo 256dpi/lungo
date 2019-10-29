36e586ae72035c943912908b55525c2cd68e4d79
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-10-29 22:02:36 +0100

    added test mode

--- bsonkit/timestamp.go
+++ bsonkit/timestamp.go
@@ -9,6 +9,7 @@ import (
 
 var tsSeconds uint32
 var tsCounter uint32
+var tsTest bool
 var tsMutex sync.Mutex
 
 // Now will generate a locally monotonic timestamp.
@@ -17,13 +18,16 @@ func Now() primitive.Timestamp {
 	tsMutex.Lock()
 	defer tsMutex.Unlock()
 
-	// get current time
-	now := uint32(time.Now().Unix())
+	// update if not in test
+	if !tsTest {
+		// get current time
+		now := uint32(time.Now().Unix())
 
-	// check if reset is needed
-	if tsSeconds != now {
-		tsSeconds = now
-		tsCounter = 1
+		// check if reset is needed
+		if tsSeconds != now {
+			tsSeconds = now
+			tsCounter = 1
+		}
 	}
 
 	// increment counter
@@ -33,4 +37,18 @@ func Now() primitive.Timestamp {
 		T: tsSeconds,
 		I: tsCounter,
 	}
+}
+
+// ResetCounter will disable the time portion of generate timestamps and reset
+// the ordinal counter to zero. This function should only be called in tests
+// to generate predictable timestamps.
+func ResetCounter() {
+	// acquire mutex
+	tsMutex.Lock()
+	defer tsMutex.Unlock()
+
+	// reset
+	tsTest = true
+	tsSeconds = 0
+	tsCounter = 0
 }
