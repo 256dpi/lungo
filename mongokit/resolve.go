e092d1e7318c3ed8aa31cf876392f34010c291a9
amreo <amreo@live.it>
2020-01-15 09:47:45 +0100

    Added support for array filters to resolve()

--- mongokit/resolve.go
+++ mongokit/resolve.go
@@ -33,10 +33,10 @@ func resolve(path string, query bsonkit.Doc, doc interface{}, arrayFilters bsonk
 
 		//TODO: implement the $ operator
 		if strings.HasPrefix(operator, "$[") && strings.HasSuffix(operator, "]") { //$[], $[<identifier>]
-			//Extract the identifier operand
-			identifier := operator[2 : len(operator)-1]
-			if identifier == "" { //$[]
-				if arr, ok := value.(bson.A); ok {
+			if arr, ok := value.(bson.A); ok {
+				//Extract the identifier operand
+				identifier := operator[2 : len(operator)-1]
+				if identifier == "" { //$[]
 					for i, _ := range arr {
 						currentPath := staticPart + "." + strconv.Itoa(i)
 						if nextPath != pathEnd && nextPath != "" {
@@ -46,11 +46,36 @@ func resolve(path string, query bsonkit.Doc, doc interface{}, arrayFilters bsonk
 							return err
 						}
 					}
-				} else {
-					return fmt.Errorf("The value pointed in the path %q isn't a array", staticPart)
+				} else { // $[<identifier>]
+					//TODO: implement array filters<identifier>
+					for i, val := range arr {
+						currentPath := staticPart + "." + strconv.Itoa(i)
+						matched := false
+						//Check if the current item match don't the arrayFilters with identifier name
+						for _, filter := range arrayFilters {
+							//TODO: Add filter checking!
+							if val, err := Match(&bson.D{
+								bson.E{Key: identifier, Value: val},
+							}, filter); err != nil {
+								return err
+							} else if val {
+								matched = true
+								break
+							}
+						}
+						if !matched {
+							continue
+						}
+						if nextPath != pathEnd && nextPath != "" {
+							currentPath += "." + nextPath
+						}
+						if err := resolve(currentPath, query, doc, arrayFilters, callback); err != nil {
+							return err
+						}
+					}
 				}
-			} else { // $[<identifier>]
-				//TODO: implement array filters
+			} else {
+				return fmt.Errorf("The value pointed in the path %q isn't a array", staticPart)
 			}
 		} else {
 			return fmt.Errorf("The operatpr %q is not supported", operator)
